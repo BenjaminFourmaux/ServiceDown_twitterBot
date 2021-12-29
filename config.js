192cfb6cafd6de3c8cef62d7d6ab0b269959ca1e
Benjamin Fourmaux <49273769+BenjaminFourmaux@users.noreply.github.com>
2021-12-29 11:34:10 +0100

    Add configuration file

---
+++ config.js
@@ -0,0 +1,12 @@
+// Change this file to config.js
+// Add your keys
+// Add file .gitignore: config.js
+// Load with 
+// var config = require('./config.js');
+
+module.exports = {
+  consumer_key:         'YOUR_CONSUMER_KEY', 
+  consumer_secret:      'YOUR_CONSUMER_SECRET',
+  access_token:         'YOUR_ACCESS_TOKEN',
+  access_token_secret:  'YOUR_ACCESS_TOKEN_SECRET'
+}
