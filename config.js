9611248c800e8a77deb2b406abb4ba784c8d4b59
Benjamin Fourmaux <49273769+BenjaminFourmaux@users.noreply.github.com>
2022-01-02 22:44:57 +0100

    Add placeholder tokens

--- config.js
+++ config.js
@@ -5,8 +5,8 @@
 // var config = require('./config.js');
 
 module.exports = {
-  consumer_key:         'oui', 
-  consumer_secret:      'oui',
-  access_token:         'oui-oui',
-  access_token_secret:  'oui'
+  consumer_key:         'YOUR_CONSUMER_KEY', 
+  consumer_secret:      'YOUR_CONSUMER_SECRET',
+  access_token:         'YOUR_ACCESS_TOKEN',
+  access_token_secret:  'YOUR_ACCESS_TOKEN_SECRET'
 }
